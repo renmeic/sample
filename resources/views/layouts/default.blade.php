e1f9c19ab4291fa6b87bc380bc9216f39e7c37bc
zhr <renmeic@163.com>
2018-08-24 15:47:11 +0000

    finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,8 +2,13 @@
 <html>
 <head>
   <title>@yield('title', 'Sample App')</title>
+  <link rel="stylesheet" href="{{ url('/css/app.css')}}">
 </head>
 <body>
-  @yield('content')
+	@include('layouts._header')
+  	<div class="container">
+     	 @yield('content')
+    </div>   
+    @include('layouts._footer')
 </body>
 </html>
