887c48c422ba83e2d4a396f5b52c3041aacabcca
zhr <renmeic@163.com>
2018-08-25 14:49:32 +0000

    add account activations

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -20,6 +20,7 @@ class UsersTableSeeder extends Seeder
         $user->email = 'zhr@zhr.com';
         $user->password = bcrypt('123123');
         $user->is_admin = true;
+        $user->activated = true;
         $user->save();
     }
 }
